c0ce6bd6d8bf260ee55b568f2cd2c6c21bc234a1
Amit <amit@amitnepal.com>
2020-09-10 20:56:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Installing please wait..."
 cp wireguard-logging.service /etc/systemd/system/wireguard-logging.service
+mkdir -p /etc/wireguard/clients/
 cp create_client.sh /etc/wireguard/
 cp wireguard-logging.sh /etc/wireguard/
 sleep 5
 chmod +x /etc/wireguard/create_client.sh
 chmod +x /etc/wireguard/wireguard-logging.sh
+systemctl enable wireguard-logging.service
+systemctl start wireguard-logging.service
 echo "Installation Complete !"
 echo "Please run ./create_client.sh to create new clients "
 echo "Log files are located at /var/log/wireguard/"
